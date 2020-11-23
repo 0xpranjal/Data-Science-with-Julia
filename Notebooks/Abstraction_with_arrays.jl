4e385d74f1d683c9215312200dda9bcef358eb4d
Bhard27 <pranjalb2709@gmail.com>
2020-11-24 00:10:14 +0530

    added array abstraction

---
+++ Notebooks/Abstraction_with_arrays.jl
@@ -0,0 +1,51 @@
+### A Pluto.jl notebook ###
+# v0.12.10
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 72ae8eac-2c80-11eb-323d-9db0e40a9140
+element = 1
+
+# ╔═╡ 60e1c6e8-2c81-11eb-1fed-7d468cbbda3c
+fill(element, 3, 4)
+
+# ╔═╡ 753bf0b4-2c81-11eb-3f72-b5a48bcc43ec
+typeof(element)
+
+# ╔═╡ 7dc9ebe6-2c81-11eb-3565-c5df3791e8a0
+typeof(fill(element, 3, 4))
+
+# ╔═╡ 29ea3bda-2ce4-11eb-1591-359cde4cb326
+md"""### Taking the element as Float now"""
+
+# ╔═╡ c3a04fc0-2c81-11eb-3592-237eb0efe200
+element1 = 1.0
+
+# ╔═╡ c37ff46e-2c81-11eb-34f2-2b3235fd5cf0
+fill(element1, 3, 4)
+
+# ╔═╡ 1a8850a0-2ce4-11eb-1ef5-d343b103f6aa
+typeof(element1)
+
+# ╔═╡ 1a6986de-2ce4-11eb-0efc-73ed618da61c
+typeof(fill(element1, 3, 4))
+
+# ╔═╡ b4832038-2cea-11eb-3e32-d96a88b62f94
+
+
+# ╔═╡ b46b3740-2cea-11eb-3141-63a988075c2e
+
+
+# ╔═╡ Cell order:
+# ╠═72ae8eac-2c80-11eb-323d-9db0e40a9140
+# ╠═60e1c6e8-2c81-11eb-1fed-7d468cbbda3c
+# ╠═753bf0b4-2c81-11eb-3f72-b5a48bcc43ec
+# ╠═7dc9ebe6-2c81-11eb-3565-c5df3791e8a0
+# ╟─29ea3bda-2ce4-11eb-1591-359cde4cb326
+# ╠═c3a04fc0-2c81-11eb-3592-237eb0efe200
+# ╠═c37ff46e-2c81-11eb-34f2-2b3235fd5cf0
+# ╠═1a8850a0-2ce4-11eb-1ef5-d343b103f6aa
+# ╠═1a6986de-2ce4-11eb-0efc-73ed618da61c
+# ╠═b4832038-2cea-11eb-3e32-d96a88b62f94
+# ╠═b46b3740-2cea-11eb-3141-63a988075c2e
