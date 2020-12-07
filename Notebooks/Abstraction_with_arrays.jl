f1cb8ef72a5ea541b95a92bb5e045399b3069898
Bhard27 <pranjalb2709@gmail.com>
2020-12-07 06:20:36 +0530

    added new commits

--- Notebooks/Abstraction_with_arrays.jl
+++ Notebooks/Abstraction_with_arrays.jl
@@ -4,6 +4,13 @@
 using Markdown
 using InteractiveUtils
 
+# ╔═╡ 6835245a-3797-11eb-3243-492891215d32
+begin
+	#using Pkg
+	#Pkg.add.(["Images", "ImageIO", "ImageMagick"])
+	using Images
+end
+
 # ╔═╡ 72ae8eac-2c80-11eb-323d-9db0e40a9140
 element = 1
 
@@ -32,20 +39,65 @@ typeof(element1)
 typeof(fill(element1, 3, 4))
 
 # ╔═╡ b4832038-2cea-11eb-3e32-d96a88b62f94
-
+md"""### Let's go ahead and take the element as a string now"""
 
 # ╔═╡ b46b3740-2cea-11eb-3141-63a988075c2e
+element_1 = "one"
 
+# ╔═╡ 10372374-376b-11eb-09b8-471832849a27
+fill(element_1, 3, 4)
+
+# ╔═╡ 0feadea6-376b-11eb-171e-498bd3eb5c50
+typeof(element_1)
+
+# ╔═╡ a205910e-376c-11eb-1a91-7109fdca5cc6
+md"""### Trying out the element as rational type"""
+
+# ╔═╡ ab22141c-377e-11eb-0510-2da7357b69dd
+element_2 = 1//1
+
+# ╔═╡ ab040a12-377e-11eb-1898-5ddb93f2d4d8
+fill(element_2, 3, 4)
+
+# ╔═╡ aae4d388-377e-11eb-0ada-e7c254780a56
+typeof(element_2)
+
+# ╔═╡ 099e7ae0-3797-11eb-09ee-fd28652f27be
+download("https://duckduckgo.com/?q=one+number&t=brave&iax=images&ia=images&iai=https%3A%2F%2F101clipart.com%2Fwp-content%2Fuploads%2F13%2FNumber%25201%2520Clipart%252018.png")
+
+# ╔═╡ 0980d8dc-3797-11eb-1f3e-9dc23a78dfbe
+
+
+# ╔═╡ 0968ee16-3797-11eb-1943-65b63e79a3c2
+
+
+# ╔═╡ a175ac2c-376c-11eb-0626-59e27c5c6dc3
+keeptrack = [typeof(1), typeof(1.0), typeof("one"), typeof(1 // 1)]
+
+# ╔═╡ a12753e4-376c-11eb-23cb-43d73f933fe8
+typeof(keeptrack)
 
 # ╔═╡ Cell order:
+# ╠═6835245a-3797-11eb-3243-492891215d32
 # ╠═72ae8eac-2c80-11eb-323d-9db0e40a9140
 # ╠═60e1c6e8-2c81-11eb-1fed-7d468cbbda3c
 # ╠═753bf0b4-2c81-11eb-3f72-b5a48bcc43ec
 # ╠═7dc9ebe6-2c81-11eb-3565-c5df3791e8a0
-# ╟─29ea3bda-2ce4-11eb-1591-359cde4cb326
+# ╠═29ea3bda-2ce4-11eb-1591-359cde4cb326
 # ╠═c3a04fc0-2c81-11eb-3592-237eb0efe200
 # ╠═c37ff46e-2c81-11eb-34f2-2b3235fd5cf0
 # ╠═1a8850a0-2ce4-11eb-1ef5-d343b103f6aa
 # ╠═1a6986de-2ce4-11eb-0efc-73ed618da61c
-# ╠═b4832038-2cea-11eb-3e32-d96a88b62f94
+# ╟─b4832038-2cea-11eb-3e32-d96a88b62f94
 # ╠═b46b3740-2cea-11eb-3141-63a988075c2e
+# ╠═10372374-376b-11eb-09b8-471832849a27
+# ╠═0feadea6-376b-11eb-171e-498bd3eb5c50
+# ╟─a205910e-376c-11eb-1a91-7109fdca5cc6
+# ╠═ab22141c-377e-11eb-0510-2da7357b69dd
+# ╠═ab040a12-377e-11eb-1898-5ddb93f2d4d8
+# ╠═aae4d388-377e-11eb-0ada-e7c254780a56
+# ╠═099e7ae0-3797-11eb-09ee-fd28652f27be
+# ╠═0980d8dc-3797-11eb-1f3e-9dc23a78dfbe
+# ╠═0968ee16-3797-11eb-1943-65b63e79a3c2
+# ╠═a175ac2c-376c-11eb-0626-59e27c5c6dc3
+# ╠═a12753e4-376c-11eb-23cb-43d73f933fe8
